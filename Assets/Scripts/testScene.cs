c1b655cba0959963d8c0b9bea506961cf36bca9f
須藤哲朗 <tetsu@sudoutetsurou-no-MacBook-Pro.local>
2015-11-08 01:35:11 +0900

    first commit

---
+++ Assets/Scripts/testScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScene : MonoBehaviour {
+
+	public int m_num;
+
+	public bool m_isFlag;
+
+	public GameObject m_Object;
+
+	public void TestFunc( int _num )
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("機会");
+
+		m_num = 0;
+
+		m_isFlag = false;
+
+		if( m_num == 0 ){
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
